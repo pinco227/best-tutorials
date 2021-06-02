3b88b9e85d0479deea4cb572c5188d9f67fdb3dc
Paul Istratoaie <p4ul.gg@gmail.com>
2021-06-02 16:39:23 +0100

    Update dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ce713520f5564c2bbf9726c75795995d888680f0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ce713520f5564c2bbf9726c75795995d888680f0',
+    ),
+  ),
+);
